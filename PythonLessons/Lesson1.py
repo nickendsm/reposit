6c65c05956288443a731e8dc8a9ea120321ee2e7
nickendsm <nickendsm@gmail.com>
2022-09-10 08:25:01 +0300

    fun_08_sept

---
+++ PythonLessons/Lesson1.py
@@ -0,0 +1,10 @@
+
+class Parrot:
+    species = 'птица'
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+kesha = Parrot("Носорог",15)
+stranniy = Parrot("Неизвестный",10)
+a = b = 10
+print(a)
